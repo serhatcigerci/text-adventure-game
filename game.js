b801cbc705bbe8b586eceb383849dc5ee4ee13c6
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-29 15:45:52 +0300

    buying a sword with goo

---
+++ game.js
@@ -0,0 +1,66 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('button')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return true
+}
+
+function selectOption(option) {
+
+}
+
+const textNodes = [
+  { 
+    id: 1, 
+    text: 'You wake up in a strange place and see a jar of blue goo near you',
+    options: [
+      {
+        text: 'Take goo.',
+        setState: { blueGoo: true},
+        nextText: 2
+      },
+      {
+        text: 'Leave the goo.',
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+    options: [
+      {
+        text: 'Trade the goo for a sword.',
+        requiredState: (currentState) => currentState.blueGoo
+        setState: { blueGoo: false, sword: true }
+      }
+    ]
+  }
+]
+
+startGame()
